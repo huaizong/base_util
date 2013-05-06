8d3bbf8a1f88d2a0e1674af0252d95af2c1d4b63
huaizong <huaizongfujian@gmail.com>
2013-05-06 23:22:13 +0800

    [fix] fix api test

--- libzhw/sample_zhw_worker_pool.c
+++ libzhw/sample_zhw_worker_pool.c
@@ -11,10 +11,10 @@ int main(int argc, char **argv)
 {
     int ret = -1;
     struct zhw_worker_pool_t *pool = zhw_worker_pool_init();
-    ret = zhw_worker_pool_create_task(pool, test_task, NULL);
+    ret = zhw_worker_pool_create_task(pool, test_task, NULL, NULL);
     long count = 1024*1024;
     while(count> 0){
-    ret = zhw_worker_pool_create_task(pool, test_task, NULL);
+    ret = zhw_worker_pool_create_task(pool, test_task, NULL, NULL);
     count--;
     }
     zhw_destroy_worker_pool(pool);
