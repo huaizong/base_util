f560be072a3844d352684681c6601f2def274c22
huaizong <huaizongfujian@gmail.com>
2013-08-08 18:04:41 +0800

    [add] add str trim func

--- libzhw/zhw_ini.h
+++ libzhw/zhw_ini.h
@@ -1,20 +1,57 @@
 #pragma once
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-#define ZHW_MAX_CONFIG_VAL_LEN 1024
-typedef _zhw_int_config_var {
-    const char *config_name;
-    int *val;
-    short must;
-}ZHW_INT_CONFIG_VAR;
 
-typedef _zhw_str_config_var {
-    const char *config_name;
+#define ZHW_MAX_CONFIG_VAL_LEN 1024
+typedef struct _zhw_ini_int32_t {
+    const char *name;
+    int32_t *val;
+    short must;
+}zhw_ini_int32_t;
+
+typedef struct _zhw_ini_uint32_t {
+    const char *name;
+    uint32_t *val;
+    short must;
+}zhw_ini_uint32_t;
+
+typedef struct _zhw_ini_int64_t {
+    const char *name;
+    int64_t *val;
+    short must;
+}zhw_ini_int64_t;
+
+typedef struct _zhw_ini_uint64_t {
+    const char *name;
+    uint64_t *val;
+    short must;
+}zhw_ini_uint64_t;
+
+typedef struct _zhw_ini_str_t {
+    const char *name;
     char val[ZHW_MAX_CONFIG_VAL_LEN+1];
     short must;
-}ZHW_INT_CONFIG_VAR;
+}zhw_ini_str_t;
+
+typedef struct _zhw_ini_dict_t {
+    unsigned n;
+    char **key;
+    char **val;
+    char *scope;
+}zhw_ini_dict_t;
+
+zhw_ini_dict_t *zhw_ini_open_(const char *file_path);
+
+const char *zhw_ini_key_map_val(
+    zhw_ini_dict_t *zhw_dict, 
+    const char *scope,
+    const char *name);
+
+void zhw_ini_close(zhw_ini_dict_t *dict);
 
 #ifdef __cplusplus
 }
