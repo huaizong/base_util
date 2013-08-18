c25450560995951c23609894fc63f7e30e3c846b
huaizong <huaizongfujian@gmail.com>
2013-08-18 10:43:41 +0800

    [tmp] tmp save

--- libzhw/zhw_ini.c
+++ libzhw/zhw_ini.c
@@ -1,1 +1,10 @@
 #include "zhw_ini.h"
+
+
+zhw_ini_dict_t *zhw_ini_open_(const char *file_path)
+{
+    if(!file_path) }
+        return NULL;
+    }
+    return NULL;
+}
--- libzhw/zhw_ini.h
+++ libzhw/zhw_ini.h
@@ -6,7 +6,6 @@ extern "C"
 {
 #endif
 
-#define ZHW_MAX_CONFIG_VAL_LEN 1024
 typedef struct _zhw_ini_int32_t {
     const char *name;
     int32_t *val;
@@ -33,7 +32,8 @@ typedef struct _zhw_ini_uint64_t {
 
 typedef struct _zhw_ini_str_t {
     const char *name;
-    char val[ZHW_MAX_CONFIG_VAL_LEN+1];
+    char *val;
+    uint32_t val_len;
     short must;
 }zhw_ini_str_t;
 
