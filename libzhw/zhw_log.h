0189b168a3b94ebf02b49fdaf1e6fa8dc720fefb
WangHuaizong <huaizongfujian@gmail.com>
2014-05-09 11:14:51 +0800

    [update] debug

--- libzhw/zhw_log.h
+++ libzhw/zhw_log.h
@@ -8,11 +8,8 @@ extern "C"
 
 #define ZHW_LOG_INFO(fmt, arg...)      zhw_write_log(LOG_INFO|LOG_LOCAL6, "%s[%d][%x]: "#fmt, __func__, __LINE__, pthread_self(), ##arg)
 #define ZHW_LOG_ERR(fmt, arg...)       zhw_write_log(LOG_ERR|LOG_LOCAL6, "%s[%d][%x]: "#fmt, __func__, __LINE__, pthread_self(), ##arg)
-#ifdef ZHW_DEBUG
-    #define ZHW_LOG_DEBUG(fmt, arg...)     zhw_write_log(LOG_DEBUG|LOG_LOCAL6, "%s[%d][%x]: "#fmt, __func__, __LINE__, pthread_self(), ##arg)
-#else
-    #define ZHW_LOG_DEBUG(fmt, arg...)   (void)(fmt, ##arg)
-#endif
+#define ZHW_LOG_DEBUG(fmt, arg...)     zhw_write_log(LOG_DEBUG|LOG_LOCAL6, "%s[%d][%x]: "#fmt, __func__, __LINE__, pthread_self(), ##arg)
+
 void zhw_log_init(const char *ident);
 void zhw_write_log(int priority, const char *message, ...);
 
